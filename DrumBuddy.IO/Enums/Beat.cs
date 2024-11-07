2947f9ec71ce2e31ad671b5da4f3e68c663f3cf4
baluka1118 <szabobazsi11182@stud.uni-obuda.hu>
2024-11-07 08:29:44 +0100

    Introduced use of DryWetMidi to connect midi devices. (WITHOUT MIDI DEVICE THIS CODE THROWS UNHANLED EXCEPTION) todo: error handling

--- DrumBuddy.IO/Enums/Beat.cs
+++ DrumBuddy.IO/Enums/Beat.cs
@@ -3,14 +3,23 @@
     public enum Beat
     {
         HiHat_Foot,
-        Bass,
-        FloorTom,
-        Snare,
-        Tom2,
-        Tom1,
-        Ride,
-        HiHat,
-        Crash,
+        Bass = 36,
+        FloorTom = 43,
+        Snare = 38,
+        Tom2 = 45,
+        Tom1 = 48,
+        Ride = 51,
+        HiHat = 42,
+        Crash1 = 49,
+        Crash2 = 457,
         Rest
     }
+    // tam1 48
+    // tam2 45
+    // alsotam 43
+    // hihat 42
+    // snare 38
+    // crash 49
+    // ride 51
+    // crash2 57
 }
--- DrumBuddy.IO/Services/MidiService.cs
+++ DrumBuddy.IO/Services/MidiService.cs
@@ -4,13 +4,36 @@ using DrumBuddy.IO.Extensions;
 using DrumBuddy.IO.Models;
 using System.Reactive.Linq;
 using DrumBuddy.IO.Abstractions;
+using LanguageExt;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Multimedia;
 
 namespace DrumBuddy.IO.Services
 {
     public class MidiService : IMidiService
     {
-        public IObservable<Beat> GetBeatsObservable(BPM tempo) //for now for testing purposes it only returns a beat every sixteenth note duration (calculated from BPM
-         => Observable.Interval(tempo.QuarterNoteDuration())
-                       .Select(_ => Beat.Snare);
+        private IInputDevice _device;
+
+        public MidiService()
+        { 
+            _device = InputDevice.GetAll().Single(); //Can be: 0 - no midi dev connected,
+                                            //1 - one dev -> connect that,
+                                            //2 - multiple devices -> user has to choose 
+            _device.StartEventsListening();
+            _midiInput = Observable.FromEventPattern<MidiEventReceivedEventArgs>(_device, nameof(_device.EventReceived))
+                .Where(ep => ep.EventArgs.Event is NoteOnEvent)
+                .Select(ep => (NoteOnEvent)ep.EventArgs.Event);
+            
+        }
+
+        private IObservable<NoteOnEvent> _midiInput;
+        
+        
+        public IObservable<Beat>
+            GetBeatsObservable(
+                BPM tempo) //for now for testing purposes it only returns a beat every sixteenth note duration (calculated from BPM
+            => _midiInput.Select(args => (Beat)int.Parse(args.NoteNumber.ToString()))
+                .Buffer(2)
+                .Select(list => list.First());
     }
 }
