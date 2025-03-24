12917aa9099387c11751de72829dd07ea37ad5e6
baluka1118 <szabobazsi11182@gmail.com>
2025-03-24 11:36:28 +0100

    Update ReactiveUI packages and improve SaveSheetViewModel validation logic

--- DrumBuddy.Client/ViewModels/Dialogs/SaveSheetViewModel.cs
+++ DrumBuddy.Client/ViewModels/Dialogs/SaveSheetViewModel.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Reactive;
-using System.Reactive.Linq;
 using System.Threading.Tasks;
 using DrumBuddy.Client.Extensions;
 using DrumBuddy.Client.Models;
 using DrumBuddy.Core.Models;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using ReactiveUI;
 using ReactiveUI.SourceGenerators;
 using ReactiveUI.Validation.Abstractions;
@@ -20,7 +17,7 @@ public partial class SaveSheetViewModel : ReactiveObject, IValidatableViewModel
     private IObservable<bool> _saveSheetCanExecute => this.IsValid();
     private readonly LibraryViewModel _library;
     private readonly SheetCreationData _sheetCreationData;
-    [Reactive] private string _sheetName;
+    [Reactive] private string _sheetName = "";
     [Reactive] private string _sheetDescription = "";
 
     public SaveSheetViewModel(SheetCreationData sheetCreationData)
@@ -33,12 +30,12 @@ public partial class SaveSheetViewModel : ReactiveObject, IValidatableViewModel
         this.ValidationRule(
             viewModel => viewModel.SheetName,
             titleObservable,
-            name =>!string.IsNullOrEmpty(name) && !_library.SheetExists(name.Trim()),
-            n =>
-            {
-                return string.IsNullOrEmpty(n) ? "Sheet title cannot be empty!" : "Sheet with this name already exists!";
-            });
+            name => !string.IsNullOrEmpty(name) && !_library.SheetExists(name.Trim()),
+            n => string.IsNullOrEmpty(n)
+                ? "Sheet title cannot be empty!"
+                : "Sheet with this name already exists!");
     }
+
     [ReactiveCommand(CanExecute = nameof(_saveSheetCanExecute))]
     private async Task SaveSheet()
     {
@@ -46,5 +43,6 @@ public partial class SaveSheetViewModel : ReactiveObject, IValidatableViewModel
         Sheet sheetToSave = new(_sheetCreationData.Bpm, _sheetCreationData.Measures, _sheetName, _sheetDescription);
         await _library.SaveSheet(sheetToSave);
     }
+
     public IValidationContext ValidationContext { get; } = new ValidationContext();
 }
