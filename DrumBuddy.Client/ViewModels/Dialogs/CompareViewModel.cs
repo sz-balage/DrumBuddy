7ddff94bd849476a542977cdec359c4e35182b6f
baluka1118 <szabobazsi11182@gmail.com>
2025-08-26 16:49:18 +0200

    Sheets can be showed in compare view in a split

--- DrumBuddy.Client/ViewModels/Dialogs/CompareViewModel.cs
+++ DrumBuddy.Client/ViewModels/Dialogs/CompareViewModel.cs
@@ -1,9 +1,40 @@
-﻿using DrumBuddy.Core.Models;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
+using DrumBuddy.Client.ViewModels.HelperViewModels;
+using DrumBuddy.Core.Models;
+using DynamicData;
 using ReactiveUI;
 
 namespace DrumBuddy.Client.ViewModels.Dialogs;
 
-public class CompareViewModel((Sheet baseSheet, Sheet comparedSheet) argInput) : ReactiveObject
+public class CompareViewModel : ReactiveObject
 {
-    
+    private readonly ReadOnlyObservableCollection<MeasureViewModel> _baseSheetMeasures;
+    private readonly SourceList<MeasureViewModel> _baseSheetMeasureSource = new();
+    public ReadOnlyObservableCollection<MeasureViewModel> BaseSheetMeasures => _baseSheetMeasures;
+
+    private readonly ReadOnlyObservableCollection<MeasureViewModel> _comparedSheetMeasures;
+    private readonly SourceList<MeasureViewModel> _comparedSheetMeasureSource = new();
+    public ReadOnlyObservableCollection<MeasureViewModel> ComparedSheetMeasures => _comparedSheetMeasures;
+    private readonly Sheet _baseSheet;
+    private readonly Sheet  _comparedSheet;
+
+    public CompareViewModel((Sheet baseSheet, Sheet comparedSheet) argInput)
+    {
+        _baseSheet = argInput.baseSheet;
+        _comparedSheet = argInput.comparedSheet;
+        _baseSheetMeasureSource.Connect()
+            .Bind(out _baseSheetMeasures)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe();
+        _comparedSheetMeasureSource.Connect()
+            .Bind(out _comparedSheetMeasures)
+            .ObserveOn(RxApp.MainThreadScheduler)
+            .Subscribe();
+        _baseSheetMeasureSource.AddRange(_baseSheet.Measures.Select(m => new MeasureViewModel(m)));
+        _comparedSheetMeasureSource.AddRange(_comparedSheet.Measures.Select(m => new MeasureViewModel(m)));
+        
+    }
 }
--- DrumBuddy.Client/Views/Dialogs/CompareView.axaml.cs
+++ DrumBuddy.Client/Views/Dialogs/CompareView.axaml.cs
@@ -1,15 +1,26 @@
-﻿using Avalonia;
+﻿using System.Reactive.Disposables;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using DrumBuddy.Client.ViewModels.Dialogs;
+using ReactiveUI;
 
 namespace DrumBuddy.Client.Views.Dialogs;
 
 public partial class CompareView : ReactiveWindow<CompareViewModel>
 {
+    private ItemsControl _baseSheetItemsControl => this.FindControl<ItemsControl>("BaseSheetItemsControl")!;
+    private ItemsControl _comparedSheetItemsControl => this.FindControl<ItemsControl>("ComparedSheetItemsControl")!;
     public CompareView()
     {
         InitializeComponent();
+        this.WhenActivated(d =>
+        {
+            this.OneWayBind(ViewModel, vm => vm.BaseSheetMeasures, v => v._baseSheetItemsControl.ItemsSource)
+                .DisposeWith(d);
+            this.OneWayBind(ViewModel, vm => vm.ComparedSheetMeasures, v => v._comparedSheetItemsControl.ItemsSource)
+                .DisposeWith(d);
+        });
     }
 }
