889c8ee76a080f1e192c77285f7a5e9fc918586f
baluka1118 <szabobazsi11182@gmail.com>
2025-10-08 08:50:08 +0200

    add TODO to handle app crashes at top level

--- DrumBuddy.Desktop/Program.cs
+++ DrumBuddy.Desktop/Program.cs
@@ -9,6 +9,7 @@ internal class Program
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
+    //TODO: handle app crashes at top level
     [STAThread]
     public static void Main(string[] args)
     {
