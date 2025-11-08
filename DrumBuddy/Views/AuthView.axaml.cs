0634fec182a6d955c30ec6544c16f5763f248b3e
sz-balage <szabobazsi11182@gmail.com>
2025-11-08 08:16:35 +0100

    enhance AuthView with gradient background and floating icons animation

--- DrumBuddy/Views/AuthView.axaml.cs
+++ DrumBuddy/Views/AuthView.axaml.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Reactive.Disposables;
 using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using Avalonia.ReactiveUI;
+using Avalonia.Threading;
 using DrumBuddy.ViewModels;
 using ReactiveUI;
 
@@ -9,6 +14,10 @@ namespace DrumBuddy.Views;
 
 public partial class AuthView : ReactiveUserControl<AuthViewModel>
 {
+    private Canvas? _canvas;
+    private List<FloatingIcon> _floatingIcons = new();
+    private Random _random = new();
+    private IDisposable? _renderLoopSubscription;
     //TODO: make this navigatable instead of switching visibility in MainWindow
     //TODO: fix layout, make prettier with gradient background etc.
     public AuthView()
@@ -17,6 +26,15 @@ public partial class AuthView : ReactiveUserControl<AuthViewModel>
 
         this.WhenActivated(d =>
         {
+            _canvas = this.FindControl<Canvas>("FloatingIconsCanvas");
+
+            if (_canvas != null)
+            {
+                _canvas.Loaded += (s, e) => InitializeFloatingIcons();
+                
+                StartAnimationLoop();
+            }
+
             this.Bind(ViewModel, vm => vm.Password, v => v.PasswordBox.Text)
                 .DisposeWith(d);
             this.Bind(ViewModel, vm => vm.Password, v => v.ConfirmPasswordBox.Text)
@@ -39,6 +57,117 @@ public partial class AuthView : ReactiveUserControl<AuthViewModel>
                     else
                         ViewModel.RegisterCommand.Execute().Subscribe();
                 };
+            d.Add(Disposable.Create(() => StopAnimationLoop()));
+
         });
+    }
+    
+    private void InitializeFloatingIcons()
+    {
+        if (_canvas == null) return;
+
+        for (int i = 0; i < 20; i++)
+        {
+            var icon = new FloatingIcon
+            {
+                X = _random.Next(0, (int)_canvas.Bounds.Width),
+                Y = _random.Next(0, (int)_canvas.Bounds.Height),
+                Size = _random.Next(40, 100),
+                Opacity = _random.Next(10, 40) / 100.0, // 0.1 to 0.4 opacity
+                SpeedX = (_random.NextDouble() - 0.5) * 2, // -1 to 1
+                SpeedY = (_random.NextDouble() - 0.5) * 2,
+                RotationSpeed = (_random.NextDouble() - 0.5) * 3,
+                Rotation = _random.Next(0, 360)
+            };
+
+            _floatingIcons.Add(icon);
+            AddIconToCanvas(icon);
+        }
+    }
+
+    private void AddIconToCanvas(FloatingIcon icon)
+    {
+        if (_canvas == null) return;
+
+        var image = new Image
+        {
+            Width = icon.Size,
+            Height = icon.Size,
+            Opacity = icon.Opacity,
+            Source = new Bitmap(AssetLoader.Open(new Uri("avares://DrumBuddy/Assets/app.ico")))
+        };
+
+        image.RenderTransform = new RotateTransform(icon.Rotation);
+
+        Canvas.SetLeft(image, icon.X);
+        Canvas.SetTop(image, icon.Y);
+
+        _canvas.Children.Add(image);
+        icon.Visual = image;
+    }
+
+    private void StartAnimationLoop()
+    {
+        if (_canvas == null) return;
+
+        var timer = new System.Threading.Timer(_ =>
+        {
+            Dispatcher.UIThread.InvokeAsync(() => UpdateAnimation());
+        }, null, 0, 16); //~60 fps
+
+        _renderLoopSubscription = Disposable.Create(() => timer.Dispose());
+    }
+
+    private void StopAnimationLoop()
+    {
+        _renderLoopSubscription?.Dispose();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (_canvas == null) return;
+
+        var width = _canvas.Bounds.Width;
+        var height = _canvas.Bounds.Height;
+
+        foreach (var icon in _floatingIcons)
+        {
+            icon.X += icon.SpeedX;
+            icon.Y += icon.SpeedY;
+
+            if (icon.X < -icon.Size)
+                icon.X = width;
+            if (icon.X > width)
+                icon.X = -icon.Size;
+
+            if (icon.Y < -icon.Size)
+                icon.Y = height;
+            if (icon.Y > height)
+                icon.Y = -icon.Size;
+
+            icon.Rotation += icon.RotationSpeed;
+            if (icon.Rotation >= 360)
+                icon.Rotation -= 360;
+
+            if (icon.Visual != null)
+            {
+                Canvas.SetLeft(icon.Visual, icon.X);
+                Canvas.SetTop(icon.Visual, icon.Y);
+                icon.Visual.RenderTransform = new RotateTransform(icon.Rotation);
+            }
+        }
+    }
+
+    private class FloatingIcon
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Size { get; set; }
+        public double Opacity { get; set; }
+        public double SpeedX { get; set; }
+        public double SpeedY { get; set; }
+        public double Rotation { get; set; }
+        public double RotationSpeed { get; set; }
+        public Image? Visual { get; set; }
     }
 }
